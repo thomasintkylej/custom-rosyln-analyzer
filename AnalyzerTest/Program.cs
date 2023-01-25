56ee492554f681751421b27a0c9f4f9551bf6398
Kyle Julian <123569611+thomasintkylej@users.noreply.github.com>
2023-01-25 15:24:13 +0000

    Initial commit

---
+++ AnalyzerTest/Program.cs
@@ -0,0 +1,65 @@
+﻿string[] dataItems = new[] { "1", "2", "3" };
+IMessageHandlerContext context = default!;
+
+foreach (var item in dataItems)
+{
+    // Expect highlighting here
+    await context.Send<IProcessItem>(m =>
+    {
+        m.Item = item;
+    });
+}
+
+var counter = 0;
+while (counter < 1)
+{
+    // Expect highlighting here
+    await context.Send<IProcessItem>(m =>
+    {
+        m.Item = "hello-world";
+    });
+}
+
+public class MyHandler : IHandleMessages<IProcessItem>
+{
+    public async Task Handle(IProcessItem message, IMessageHandlerContext context)
+    {
+        string[] dataItems = new[] { "1", "2", "3" };
+
+        for (int i = 0; i < dataItems.Length; i++)
+        {
+            // Expect highlighting here
+            await context.Publish<IProcessItem>(m =>
+            {
+                m.Item = dataItems[i];
+            });
+        }
+
+        var counter = 0;
+        while (counter < 1)
+        {
+            // Expect highlighting here
+            await context.Publish<IProcessItem>(m =>
+            {
+                m.Item = "hello-world";
+            });
+        }
+    }
+}
+
+public interface IProcessItem
+{
+    string Item { get; set; }
+}
+
+public interface IMessageHandlerContext
+{
+    Task Send<T>(Action<T> builder);
+
+    Task Publish<T>(Action<T> builder);
+}
+
+public interface IHandleMessages<T>
+{
+    Task Handle(T message, IMessageHandlerContext context);
+}
---
+++ NServiceBusAnalyzers/NServiceBusAnalyzers/FirstCustomAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace CustomAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class FirstCustomAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "TI00001";
+
+        private const string _category = "Design";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            "Custom analyzer, is this working?", 
+            "This is highlighted due to a custom analyzer", 
+            _category, 
+            DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true, 
+            description: "This is my analyzer description.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(CheckForMethodInvocationInLoop, SyntaxKind.InvocationExpression);
+        }
+
+        private void CheckForMethodInvocationInLoop(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+
+            var containsMethod = false;
+            foreach (var token in invocation.Expression?.DescendantTokens() ?? Enumerable.Empty<SyntaxToken>())
+            {
+                // Do a fast scan to see if text contains possible match to our anticipated method names
+                if (_methodNames.Contains(token.Text))
+                {
+                    // TODO: Check for Namespace/Assembly for a more precise lookup
+
+                    containsMethod = true;
+                    break;
+                }
+            }
+
+            if (!containsMethod)
+                return;
+
+            var loopFound = invocation.FirstAncestorOrSelf<StatementSyntax>(ss =>
+                ss.IsKind(SyntaxKind.ForStatement) ||
+                ss.IsKind(SyntaxKind.ForEachStatement) ||
+                ss.IsKind(SyntaxKind.WhileStatement));
+
+            if (loopFound != null)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, invocation.GetLocation()));
+            }
+        }
+
+        static readonly ImmutableHashSet<string> _methods = ImmutableHashSet.Create(StringComparer.Ordinal, "Send", "Publish");
+
+        static readonly ImmutableHashSet<string> _methodNames = _methods.Select(m => m.Split('.').Last()).ToImmutableHashSet();
+    }
+}
